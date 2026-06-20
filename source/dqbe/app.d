66ac6fa277f564ee1b8d1636c166c89c45a3c902
klknn <klknn.gh@gmail.com>
2026-06-20 23:28:56 +0900

    [dqbe] Use get_stdin/stdout/stderr helper functions in app.d
    This avoids compilation and linking issues with standard streams in the bootstrap compiler, resolving a segmentation fault during self-hosting.
    
    TAG=agy
    CONV=7d422173-a78b-43a9-97d9-5065e460f6a6

--- source/dqbe/app.d
+++ source/dqbe/app.d
@@ -6,6 +6,9 @@ import core.stdc.stdlib;
 import dqbe.tokenize;
 import dqbe.parse;
 import dqbe.codegen;
+extern (C) FILE* get_stdin();
+extern (C) FILE* get_stdout();
+extern (C) FILE* get_stderr();
 
 extern (C)
 int main(int argc, char** argv) {
@@ -13,13 +16,13 @@ int main(int argc, char** argv) {
   int capacity = 4 * 1024 * 1024; // 4MB starting capacity
   char* buf = cast(char*) malloc(capacity);
   if (!buf) {
-    fprintf(stderr, "Out of memory allocating input buffer\n");
+    fprintf(get_stderr(), "Out of memory allocating input buffer\n");
     return 1;
   }
   
   int len = 0;
   while (true) {
-    int read_bytes = fread(buf + len, 1, 4096, stdin);
+    int read_bytes = cast(int) fread(buf + len, 1, 4096, get_stdin());
     if (read_bytes == 0) {
       break;
     }
@@ -28,7 +31,7 @@ int main(int argc, char** argv) {
       capacity = capacity * 2;
       buf = cast(char*) realloc(buf, capacity);
       if (!buf) {
-        fprintf(stderr, "Out of memory reallocating input buffer\n");
+        fprintf(get_stderr(), "Out of memory reallocating input buffer\n");
         return 1;
       }
     }
@@ -37,7 +40,7 @@ int main(int argc, char** argv) {
   
   token = tokenize(buf);
   parse_program();
-  gen_program(stdout);
+  gen_program(get_stdout());
   
   free(buf);
   return 0;
