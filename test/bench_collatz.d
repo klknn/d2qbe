52fa983a55d277ffa215fde241923bb2fd40ff98
klknn <klknn.gh@gmail.com>
2026-06-22 20:52:31 +0900

    Rewrite Collatz benchmark using 32-bit int with 100k limit and 100 reps

--- test/bench_collatz.d
+++ test/bench_collatz.d
@@ -1,34 +1,37 @@
-extern (C) void* malloc(long size);
+extern (C) void* malloc(int size);
 extern (C) int printf(const char* format, ...);
 
 extern (C) int main() {
-    long n;
-    long nv;
-    long c;
-    long cmax;
-    long* mem;
+    int n;
+    int nv;
+    int c;
+    int cmax;
+    int* mem;
+    int rep;
 
-    mem = cast(long*) malloc(8 * 1000000);
+    mem = cast(int*) malloc(4 * 100000);
 
     cmax = 0;
-    for (nv = 1; nv < 1000000; nv++) {
-        n = nv;
-        c = 0;
-        while (n != 1) {
-            if (n < nv) {
-                c = c + mem[n];
-                break;
+    for (rep = 0; rep < 100; rep++) {
+        for (nv = 1; nv < 100000; nv++) {
+            n = nv;
+            c = 0;
+            while (n != 1) {
+                if (n < nv) {
+                    c = c + mem[n];
+                    break;
+                }
+                if (n & 1)
+                    n = 3 * n + 1;
+                else
+                    n = n / 2;
+                c++;
             }
-            if (n & 1)
-                n = 3 * n + 1;
-            else
-                n = n / 2;
-            c++;
+            mem[nv] = c;
+            if (c > cmax)
+                cmax = c;
         }
-        mem[nv] = c;
-        if (c > cmax)
-            cmax = c;
     }
-    printf("max steps: %ld\n", cmax);
+    printf("max steps: %d\n", cmax);
     return 0;
 }
